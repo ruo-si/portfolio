c50883deca1fee54fa5079201bc0bec6f3a65022
Kuanting <kuanting@kuan-tings-imac.myfiosgateway.com>
2021-04-04 21:14:01 -0400

    home skeloton wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import home from "./pages/home";
+import Footer from "./components/footer";
+import TopNav from "./components/topNav";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <TopNav />
+        <Route exact path="/" component={home} />
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/topNav.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class TopNav extends Component {
+  render() {
+    return <div>
+        <div>Victoria Lee</div>
+        <ul>
+           <li>Portfolio</li>
+           <li>About</li> 
+           <li>Press</li>
+        </ul>
+        </div>;
+  }
+}
+
+export default TopNav;
