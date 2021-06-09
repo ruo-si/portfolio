286038217a5394120e0c5165bfc52d14107b61c8
Kuanting <pankuanting@gmail.com>
2021-06-08 23:28:36 -0400

    about css

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,63 +1,54 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/grid"
-
+import emailjs from "emailjs-com";
+import arrow from "../assets/link_out.svg";
+import "./about.css";
 const About = () => {
+  function sendEmail(e) {
+    e.preventDefault();
 
-return (
+    emailjs
+      .sendForm(
+        "service_zxznxs4",
+        "template_38kexia",
+        e.target,
+        "user_U555spLCYhH6rvOSPbjrU"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
 
-    <div>
-        <div>
-
-            Hi I am Victoria, I am a full stack web developer and result driven creative designer in 2D + 3D  visual communications, virtual reality and interactive content strategy. A motivated team player passionate about project management, storytelling and problem solving with code and design in a collaborative multicultural work environment.
-             
+  return (
+    <div className="about-wrap">  
+    <div className="container"> 
+      <div className="intro-wrap">
+        <div className="module-title"> About</div>
+        <div className="intro-text">
+          Hi I am Victoria, I am a full stack web developer and result driven
+          creative designer in 2D + 3D visual communications, virtual reality
+          and interactive content strategy. A motivated team player passionate
+          about project management, storytelling and problem solving with code
+          and design in a collaborative multicultural work environment.
         </div>
-
-    <Container>
-        <Row>
-         <Col size="6">
-             <form>
-                <input
-                    // value={this.state.email}
-                    // onChange={this.handleInputChange}
-                    name="subject"
-                    placeholder="Subject"
-                />
-
-                 <input
-                    // value={this.state.password}
-                    // onChange={this.handleInputChange}
-                    name="name"
-                    placeholder="Your Name"
-                />
-
-                <input
-                    // value={this.state.password}
-                    // onChange={this.handleInputChange}
-                    name="email"
-                    placeholder="Email Address"
-                />
-
-                <input
-                    // value={this.state.password}
-                    // onChange={this.handleInputChange}
-                    name="message"
-                    placeholder="Message"
-                />
-
-                <Link to="/about">
-
-                    <button> Submit </button>
-
-                </Link>
-            </form>
-        </Col>
-    </Row>
-</Container>
-
-</div>
-)
-}
-
+      </div>
+    
+      <form onSubmit={sendEmail}>
+        <input type="text" name="subject" placeholder="Subject" />
+        <input type="text" name="name" placeholder="Your Name" />
+        <input type="email" name="email" placeholder="Email" />
+        <textarea name="message" placeholder="Your Message" />
+        <button type="submit" value="Send">Submit
+        <img alt="linkout" src={arrow} class="linkout" />
+        </button> 
+      </form>
+      </div>
+    </div>
+  );
+};
 
 export default About;
