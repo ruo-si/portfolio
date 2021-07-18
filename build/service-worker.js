aae262326e42feede06d2d198d3b5c431280342c
Victoria Lee <victoriacmlee@gmail.com>
2021-07-18 06:55:58 -0400

    prepare build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.34e70f674b465b7139152021b176996d.js"
+  "/portfolio/precache-manifest.6151fca65f8bd9b76d9207ff75879b45.js"
 );
 
 workbox.clientsClaim();
