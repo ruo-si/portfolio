01bebc27d337896f636660761f0d248d92e9d230
Victoria Lee <victoriacmlee@gmail.com>
2021-09-17 04:01:05 -0400

    fix footer and landing code

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,33 +5,38 @@ import mail from "../assets/mail.svg";
 import scroll from "../assets/scroll.svg";
 
 const Footer = () => {
-  const [show, setShow] = useState(true)
-  const controlNavbar = () => {
-      if (window.scrollY > 100) {
-          setShow(false)
-      } else {
-          setShow(true)
-      }
-  }
+  // const [show, setShow] = useState(true)
+  // const controlNavbar = () => {
+  //   if (window.scrollY > 100) {
+  //     setShow(false)
+  //   } else {
+  //     setShow(true)
+  //   }
+  // }
 
-  useEffect(() => {
-      window.addEventListener('scroll', controlNavbar)
-      return () => {
-          window.removeEventListener('scroll', controlNavbar)
-      }
-  }, [])
+  // useEffect(() => {
+  //   window.addEventListener('scroll', controlNavbar)
+  //   return () => {
+  //     window.removeEventListener('scroll', controlNavbar)
+  //   }
+  // }, [])
+
   return (
+
     <footer>
       <div className="scroll">
-      <img alt="scroll down" src={scroll} />
+        <img alt="scroll down" src={scroll} />
       </div>
+
       <Link to="/about">
         {" "}
         <div >
-        <img alt="img" src={mail} className="mail"/>
+          <img alt="img" src={mail} className="mail" />
         </div>
       </Link>
+
     </footer>
+
   );
 };
 
