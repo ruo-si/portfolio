6a71b14d2ab7b89d6974228a16187afd2e3693de
Victoria Lee <victoriacmlee@gmail.com>
2021-09-20 19:19:23 -0400

    router and pages setup for deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+
+// navigation
+import Home from "./pages/home";
+import Footer from "./components/footer";
+import TopNav from "./components/topNav";
+import About from "./pages/about"
+import Press from "./pages/press"
+
+// ux projects
+import LT from "./pages/pd-lt"
+import CONFIGURATOR from "./pages/ww-configurator"
+
+// rendering projects
+import WW_TOKYO from "./pages/pd-tokyo"
+import SHoP from "./pages/pd-shop"
+import WW_COLUMBIA from "./pages/pd-colombia"
+
+// dev projects
+import DEV_WASTE_OF_TIME from "./pages/pd-WasteOfTime"
+
+// vr projects
+// import VR_CHICAGO from "./pages/vr-chicago"
+// import VR_DC from "./pages/vr-dc"
+// import VR_LA from "./pages/vr-la"
+// import VR_SEOUL from "./pages/vr-seoul"
+
+// vr 360
+// import VR_CHICAGO_360 from "./pages/vr-chicago-360"
+// import VR_DC_360 from "./pages/vr-dc-360"
+// import VR_LA_360 from "./pages/vr-la-360"
+// import VR_SEOUL_360 from "./pages/vr-seoul-360"
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+
+      <div>
+
+        <TopNav />
+
+        {/* navigation */}
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/press" component={Press} />
+
+        {/* ux projects */}
+        <Route exact path="/configurator" component={CONFIGURATOR} />
+        <Route exact path="/lt" component={LT} />
+
+        {/* UX & dev projects */}
+        <Route exact path="/waste-of-time" component={DEV_WASTE_OF_TIME} />
+
+        {/* 3d projects */}
+        <Route exact path="/ww-tokyo" component={WW_TOKYO} />
+        <Route exact path="/shop" component={SHoP} />
+        <Route exact path="/ww-colombia" component={WW_COLUMBIA} />
+
+        {/* vr projects */}
+        {/* <Route exact path="/vr-chicago" component={VR_CHICAGO} />
+        <Route exact path="/vr-dc" component={VR_DC} />
+        <Route exact path="/vr-la" component={VR_LA} />
+        <Route exact path="/vr-seoul" component={VR_SEOUL} /> */}
+
+        {/* vr 360 */}
+        {/* <Route exact path="/vr-chicago-360" component={VR_CHICAGO_360} />
+        <Route exact path="/vr-dc-360" component={VR_DC_360} />
+        <Route exact path="/vr-la-360" component={VR_LA_360} />
+        <Route exact path="/vr-seoul-360" component={VR_SEOUL_360} /> */}
+
+        <Footer />
+
+      </div>
+
+    </Router>
   );
 }
 
---
+++ src/pages/pd-shop.js
@@ -0,0 +1,119 @@
+import React from "react";
+import "./pd.css";
+import ReactCompareImage from "react-compare-image";
+
+// hero imagery
+import DAY from "../assets/SHOP_FALL.jpg";
+import NIGHT from "../assets/SHOP_WINTER.jpg";
+
+// project imagery
+import H1 from "../assets/SHOP_2.jpg";
+import H2 from "../assets/SHOP_1.jpg";
+import H3 from "../assets/SHOP_3.jpg";
+
+// related projects
+import R1 from "../assets/H_3.jpg";
+import R2 from "../assets/H_5.jpg";
+
+
+const Project_SHoP= () => {
+
+  return (
+
+    <div className="pd-container">
+
+      <div className="project-detail-wrap">
+
+        {/* title */}
+        <div className="project-name">SHoP Architects</div>
+
+        {/* project info */}
+        <div className="project-credit">
+          <div>Firm: SHoP Architects</div>
+          <div>Visualization Director: Mengyi Fan</div>
+          <div>Interactive Visualization Designer: Victoria Lee</div>
+        </div>
+        
+        {/* project description */}
+        <div className="project-detail">
+          <div>Contribution:</div>
+          <div>Art Direction, Visual Experience</div>
+          <div>Tool:</div>
+          <div>3DsMax, V-Ray Render Engine, Forrest Pack, Adobe Suite</div>
+          {/* <div>Timeframe:</div> */}
+
+          <div>At ShoP Architects, I worked on a wide variety of projects in residential, hospitality, commercial and office spaces. I was able to participate in the process of visualizing projects for clients and investors to engage and realize the projects. My work focuses on using 3D and interactive strategy to help the projects come to life and tell a story before it is celebrated.</div>
+        </div>
+
+      </div>
+
+      {/* buttons */}
+      <div>
+        <button>Dusk</button>
+        <button>Morning</button>
+        <button>Evening</button>
+        <button>Midnight</button>
+      </div>
+
+      {/* compare image */}
+      <div className="head-wrap">
+
+        <ReactCompareImage
+          leftImage={DAY}
+          rightImage={NIGHT}
+          horizontal
+        />
+
+        {/* caption */}
+        <div className="caption">Imagery for the proposal of the Obama Presidential Library for SHoP Architects, placed 4th place in the selection process. A event space that encourages activity throughout the seasons enjoyed by the local community.</div>
+
+      </div>
+
+      <div className="text-img-wrap">
+
+        <img alt="img" src={H1} className="image-wrap-left" />
+
+        <div className="text-wrap-right">
+          A winning project bidding visualization for Fulbright University in Vietnam. 
+        </div>
+      </div>
+
+      <div className="text-img-wrap">
+        <div className="text-wrap-left">
+          A built project: affordable and efficient apartment building located in Brooklyn, New York. The model is built in Rhino paired with Grasshopper and modified in 3DsMax and rendered with V-Ray Rendering Engine.
+        </div>
+        <img alt="img" src={H2} className="image-wrap-right" />
+      </div>
+
+      <div className="head-wrap">
+        <img alt="img" src={H3} className="img-wrap" />
+        <div className="caption">A visual proposal for a Silicon Valley Tech headquarters.</div>
+
+      </div>
+
+      <div className="cross-wrap">
+        <div className="cross-title">Related Projects</div>
+        <div className="thumb-left">
+          <img alt="img" src={R1} className="thumb" />
+          <div className="project-title">
+            WeWork
+            <br />
+            Shenzhen
+          </div>
+        </div>
+
+        <div className="thumb-right">
+          <img alt="img" src={R2} className="thumb" />
+          <div className="project-title">
+            WeWork
+            <br />
+            Shenzhen
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Project_SHoP;
